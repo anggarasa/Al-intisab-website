d80439fe997056b4c785745f89a22cbe49da1180
reihanariansyah71 <reihanariansyah71@gmail.com>
2025-01-08 13:18:43 +0700

    install laravel breze

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "hello world";
-});
+Route::view('/', 'welcome');
 
-Route::get('/about', function () {
-    echo "about us";
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
